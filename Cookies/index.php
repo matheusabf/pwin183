e871560e2a6ca7ff77d114aa0f97b020055ef16e
matheusabf <48792317+matheusabf@users.noreply.github.com>
2020-06-29 23:24:27 -0300

    Update index.php

--- Cookies/index.php
+++ Cookies/index.php
@@ -4,7 +4,7 @@
     <body>
         <form class="" action="cookies.php">
            <h1> Digite seus dados abaixo:</h1>
-            <p><input type="text" name="nome" placeholder="Nome"></p>
+            <p><input type="text" name="name" placeholder="Nome"></p>
             <p><input type="text" name="cidade" placeholder="Cidade de Nascimento"></p> 
             <p><input type="text" name="email" placeholder="Email"> </p> 
             <input type="submit" value="enviar">
